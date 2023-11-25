3e66979aae4dcbbc79afa7ec032b77c7af30e754
ireongi <ireongi@gmail.com>
2023-11-25 18:10:18 +0900

    week13 : 댓글 작성

--- routes/posts.js
+++ routes/posts.js
@@ -9,4 +9,14 @@ router.get('/insert', function(req, res){
     res.render('index', {title : '글쓰기', pageName:'posts/insert.ejs'})
 });
 
+router.get('/read', function(req, res){
+    const id=req.query.id;
+    res.render('index', {title : '게시글 정보', pageName:'posts/read.ejs', id})
+});
+
+router.get('/update', function(req, res){
+    const id=req.query.id;
+    res.render('index', {title : '게시글 수정', pageName:'posts/update.ejs', id})
+});
+
 module.exports = router;
